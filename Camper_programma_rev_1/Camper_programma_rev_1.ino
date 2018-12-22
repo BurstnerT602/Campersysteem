e6c2deec01ba1c696debf49bbfa36db2c5a1af1b
Geert Huizing <geerthuizing@air-van-geert.home>
2018-12-22 17:29:58 +0100

    Spanningsmeting Accu's toegevoegd
    Spanningsmeting van start en woonaccu toegevoegd.

--- Camper_programma_rev_1/Camper_programma_rev_1.ino
+++ Camper_programma_rev_1/Camper_programma_rev_1.ino
@@ -13,43 +13,43 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-// inlezen, aansturen drinkwatertank.
-
 // ingangen van de drinkwatertank
-    const int drinkw25Pin = 2;      // drinkwater 25%
-    const int drinkw50Pin = 3;      // drinkwater 50%
-    const int drinkw75Pin = 4;      // drinkwater 75%
-    const int drinkw100Pin = 5;     // drinkwater 100%
-  
-// aansturing leds drinkwatertank 
-//  const int led25Pin =  22;      // the number of the LED25 pin
-//  const int led50Pin =  23;      // the number of the LED50 pin
-//  const int led75Pin =  24;      // the number of the LED75 pin
-//  const int led100Pin =  25;     // the number of the LED100 pin
+const int drinkw25Pin = 2;      // drinkwater 25%
+const int drinkw50Pin = 3;      // drinkwater 50%
+const int drinkw75Pin = 4;      // drinkwater 75%
+const int drinkw100Pin = 5;     // drinkwater 100%
 
 // variables will change:
-    int drinkw25State = 0;         // variable for reading the drinkwater 25% status
-    int drinkw50State = 0;         // variable for reading the drinkwater 50% status
-    int drinkw75State = 0;         // variable for reading the drinkwater 75% status
-    int drinkw100State = 0;        // variable for reading the drinkwater 100% status
+int drinkw25State = 0;         // variable for reading the drinkwater 25% status
+int drinkw50State = 0;         // variable for reading the drinkwater 50% status
+int drinkw75State = 0;         // variable for reading the drinkwater 75% status
+int drinkw100State = 0;        // variable for reading the drinkwater 100% status
 
-// inlezen, aansturen grijswatertank.
+// ingangen accuspanning start en woonaccu
+const int startaccuPin = A3;   // spanning startaccu
+const int woonaccuPin = A4;    // spanning woonaccu
+float vIn;
+float vOut;
+float startaccuVal;
+float woonaccuVal;
+const float factor = 4.092;
+const float vCC = 5.00;
 
 // ingangen van de grijswatertank
-    const int grijsw25Pin = 6;      // grijswater 25%
-    const int grijsw50Pin = 7;      // grijswater 50%
-    const int grijsw75Pin = 8;      // grijswater 75%
-    const int grijsw100Pin = 9;     // grijswater 100%
+const int grijsw25Pin = 6;      // grijswater 25%
+const int grijsw50Pin = 7;      // grijswater 50%
+const int grijsw75Pin = 8;      // grijswater 75%
+const int grijsw100Pin = 9;     // grijswater 100%
 
 // variables will change:
-    int grijsw25State = 0;         // variable for reading the drinkwater 25% status
-    int grijsw50State = 0;         // variable for reading the drinkwater 50% status
-    int grijsw75State = 0;         // variable for reading the drinkwater 75% status
-    int grijsw100State = 0;        // variable for reading the drinkwater 100% status
+int grijsw25State = 0;         // variable for reading the drinkwater 25% status
+int grijsw50State = 0;         // variable for reading the drinkwater 50% status
+int grijsw75State = 0;         // variable for reading the drinkwater 75% status
+int grijsw100State = 0;        // variable for reading the drinkwater 100% status
 
 void WaterStatus(int state, int regel1, int regel2, String tekst){
   if (state == HIGH) {
-    lcd.clear();
+    //lcd.clear();
     lcd.setCursor(regel1, regel2);    
     lcd.print (tekst);
   }
@@ -57,17 +57,22 @@ void WaterStatus(int state, int regel1, int regel2, String tekst){
 
     
 void setup() {
-Serial.begin(9600);
-    lcd.init();
-    lcd.backlight();
-    lcd.clear();
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
  
   
-  // initialize the pushbutton pins as an input:
-    pinMode(drinkw25Pin, INPUT);
-    pinMode(drinkw50Pin, INPUT);
-    pinMode(drinkw75Pin, INPUT);
-    pinMode(drinkw100Pin, INPUT);
+  // initialize watertank sensors as an input:
+  pinMode(drinkw25Pin, INPUT);
+  pinMode(drinkw50Pin, INPUT);
+  pinMode(drinkw75Pin, INPUT);
+  pinMode(drinkw100Pin, INPUT);
+  
+  pinMode(grijsw25Pin, INPUT);
+  pinMode(grijsw50Pin, INPUT);
+  pinMode(grijsw75Pin, INPUT);
+  pinMode(grijsw100Pin, INPUT);
 }
 
 void loop() {
@@ -77,16 +82,20 @@ void loop() {
   drinkw50State = digitalRead(drinkw50Pin);
   drinkw75State = digitalRead(drinkw75Pin);
   drinkw100State = digitalRead(drinkw100Pin);
+  grijsw25State = digitalRead(drinkw25Pin);
+  grijsw50State = digitalRead(drinkw50Pin);
+  grijsw75State = digitalRead(drinkw75Pin);
+  grijsw100State = digitalRead(drinkw100Pin);
   
   // check niveau drinkwatertank:
-  while (drinkw25State == LOW)  {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    delay(500);
-    lcd.print ("Drinkwater LEEG");
-    delay(1000);
-    drinkw25State = digitalRead(drinkw25Pin);
-  }
+//  while (drinkw25State == LOW)  {
+//    lcd.clear();
+//    lcd.setCursor(0,0);
+//    delay(500);
+//    lcd.print ("Drinkwater LEEG");
+//    delay(1000);
+//    drinkw25State = digitalRead(drinkw25Pin);
+//  }
 
   WaterStatus(drinkw25State, 0, 0, "Drinkwater 25%");
 
@@ -97,14 +106,14 @@ void loop() {
   WaterStatus(drinkw100State, 0, 0, "Drinkwater 100%");
   
 // check niveau grijswatertank
-  while (grijsw25State == LOW)  {
-    lcd.clear();
-    lcd.setCursor(0,1);
-    delay(500);
-    lcd.print ("Grijswater LEEG");
-    delay(1000);
-    drinkw25State = digitalRead(drinkw25Pin);
-  }
+//  while (grijsw25State == LOW)  {
+//    lcd.clear();
+//    lcd.setCursor(0,1);
+//    delay(500);
+//    lcd.print ("Grijswater LEEG");
+//    delay(1000);
+//    drinkw25State = digitalRead(drinkw25Pin);
+//  }
   
   WaterStatus(grijsw25State, 0, 1, "Grijswater 25%");
 
