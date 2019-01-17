5354e9c438243078b45c4728f293786b80ae82ba
Geert Huizing <geerthuizing@192.168.2.8>
2019-01-17 23:44:43 +0100

    laden / ontladen accu toegevoegd
    toegevoegd:
    laden woonaccu
    ontladen woonaccu

--- Camper-project/Camper-project.ino
+++ Camper-project/Camper-project.ino
@@ -1,24 +1,11 @@
 /*
-  PIN AANSLUITINGEN Ultrasoon:
- *
- * Vcc  --> 5V
- * Trig --> 12
- * Echo --> 8
- * GND  --> GND
- * 
- * * Walstroom --> pin30
- * 
+  
  * Gegevens drinkwaterwatertank
  * hoogte = 40cm
  * inhoud is 80 ltr.
  * factor: 80/40 = per centimeter 2 ltr.
  * 
- * PIN AANSLUITINGEN Voltmeter:
- * * S --> A3 en A4 groen
- * + --> 5V  rood
- * - --> GND blauw
- * 
- * Exacte waarden berekenen met multimeter:
+ * Exacte waarden spanningen berekenen met multimeter:
  * Vin op adapter = 12.41V
  * Vout op Arduino = 2.497V
  * 5V op Arduino = 5.15V
@@ -26,7 +13,6 @@
  * 12.41 / 2.497 = 4.97
  */
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <WaterTank.h>
 #include <SimpleDHT.h>
 #include <DS3231.h>
@@ -34,30 +20,44 @@
 #include <nextionlib.h>
 
 
-
-WaterTank vuilWaterTank = WaterTank(22,24,26,28);
+WaterTank vuilWaterTank = WaterTank(6, 7, 8, 9);
 DS3231  rtc(SDA, SCL);
 NextionLib Display;
 
 //=============================================================
 // Ultrasoon drinkwatertank
-const int trigPin = 12;   // trigger pin
-const int echoPin = 8;    // echo in
+const int trigPin = 4;    // trigger pin
+const int echoPin = 5;    // echo in
+int   duration;           // variabele voor tijdsduur als kommagetal
+int   distance;           // variabele voor afstand als kommagetal
 
-int   duration;           // variabele voor tijdsduur als kommagetal (float)
-int   distance;           // variabele voor afstand als kommagetal (float)
+//=============================================================
+// Vuilwatertank.
+int counter = 0;
+int VwaterPin = 10;
+
+//==============================================================
+// Koelkast
+int KkastPin = 12;
 
 //==============================================================
 // Accuspanning Woonaccu
-const int woonaccuSensorPin = A3;          // sensor pin
-float vwoonIn;                                // gemeten voltage (3.3V = max. 16.5V, 5V = max 25V)
+const int woonaccuSensorPin = A1;          // sensor pin
+float vwoonIn;                             // gemeten voltage (3.3V = max. 16.5V, 5V = max 25V)
 float vwoonOut;
 float woonaccuSensorVal;                   // waarde op pin A3 (0 - 1023)
-const float factorwoonaccu = 4.092;               // reductie factor van het Voltage Sensor shield
+const float factorwoonaccu = 4.092;        // reductie factor van het Voltage Sensor shield
+
+//==============================================================
+// Laden / ontladen woonaccu
+const int IwooninPin = A3;
+const int IwoonoutPin = A4;
+float AwoonIn;
+float AwoonOut;
 
 //================================================================
 // Accuspanning Startaccu
-const int startaccuSensorPin = A4;          // sensor pin
+const int startaccuSensorPin = A2;          // sensor pin
 float vstartIn;                             // gemeten voltage (3.3V = max. 16.5V, 5V = max 25V)
 float vstartOut;
 float startaccuSensorVal;                   // waarde op pin A4 (0 - 1023)
@@ -69,7 +69,7 @@ const float vCC = 5.15;                     // Arduino invoer voltage (na te met
 
 //================================================================
 // Aanwezigheid walstroom
-const int WalstroomPin = 30;
+const int WalstroomPin = 11;
 int WalstroomState = 0;
 
 //=================================================================
@@ -78,96 +78,134 @@ int pinDHT11binnen = 2;
 int pinDHT11buiten = 3;
 SimpleDHT11 dht11;
 
+//=================================================================
+// LPG tank
+const int gaslevelPin = A5;
+float gaslevel;
+int gasnivo;
+
+//=================================================================
+
 void setup() {                
-  Serial.begin(9600);
-  rtc.begin();
-  pinMode(trigPin, OUTPUT);                  // stel de trigger pin in als uitvoer drinkwatertank
-  pinMode(echoPin, INPUT);                   // stel de echo pin in als invoer
- 
+    Serial.begin(9600);
+    rtc.begin();
+    pinMode(trigPin, OUTPUT);                  // stel de trigger pin in als uitvoer drinkwatertank
+    pinMode(echoPin, INPUT);                   // stel de echo pin in als invoer
+    pinMode(VwaterPin, OUTPUT);
+    pinMode(KkastPin, OUTPUT);
+    digitalWrite(VwaterPin, LOW);
 }
   
 void loop() {
-  // Uitlezen Drinkwatertank.
-  digitalWrite(trigPin, LOW);       
-  delayMicroseconds(2);            
+ 
+//   Drinkwatertank.
+    digitalWrite(trigPin, LOW);       
+    delayMicroseconds(2);            
 
-  digitalWrite(trigPin, HIGH);      
-  delayMicroseconds(10);            
-  digitalWrite(trigPin, LOW);       
+    digitalWrite(trigPin, HIGH);      
+    delayMicroseconds(10);            
+    digitalWrite(trigPin, LOW);       
 
-  duration = pulseIn(echoPin, HIGH);                                    
-  distance = (duration * 0.0343) / 2; 
-  Display.Send(80-(distance * 2), "Drinkw");
-
+    duration = pulseIn(echoPin, HIGH);                                    
+    distance = (duration * 0.0343) / 2; 
+    Display.Send(80-(distance * 2), "Dwater");
     
-   //Uitlezen Vuilwatertank.
-  WaterTankStatus vuilWaterTankStatus = vuilWaterTank.GetStatus(); 
-  switch(vuilWaterTankStatus){
-    case WaterTankStatus::Vol:
-      Display.Send("100", "Gwater", "63844");
-    break;
-    case WaterTankStatus::DrieKwart:
-      Display.Send("75-100", "Gwater", "64800");
-    break;
-    case WaterTankStatus::Half:
-      Display.Send("50-75", "Gwater", "2016");
-    break;
-    case WaterTankStatus::Kwart:
-      Display.Send("25-50", "Gwater", "2016");
-    break;
-    case WaterTankStatus::Leeg:
-      Display.Send("0-25", "Gwater", "2016");  
-    break;
-  }
+//   Vuilwatertank.
+    counter++ ;
+     if(counter == 3600); {
+      digitalWrite(VwaterPin, HIGH);
+     
+     WaterTankStatus vuilWaterTankStatus = vuilWaterTank.GetStatus(); 
+      switch(vuilWaterTankStatus){
+      case WaterTankStatus::Vol:
+          Display.Send("100", "Gwater", "63844");
+      break;
+      case WaterTankStatus::DrieKwart:
+          Display.Send("75-100", "Gwater", "64800");
+      break;
+      case WaterTankStatus::Half:
+          Display.Send("50-75", "Gwater", "2016");
+      break;
+      case WaterTankStatus::Kwart:
+          Display.Send("25-50", "Gwater", "2016");
+       break;
+       case WaterTankStatus::Leeg:
+          Display.Send("0-25", "Gwater", "2016");  
+       break;
+      delay(1000);
+      counter=0;
+      }
+   }
+//      Woonaccuspanning
+       woonaccuSensorVal = analogRead(woonaccuSensorPin);       // lees de waarde van de sensor (0 - 1023) 
+       vwoonOut = (woonaccuSensorVal / 1024) * vCC;             // converteer de gelezen waarde naar het daadwerkelijke voltage op de analoge pin
+       vwoonIn =  vwoonOut * factorwoonaccu;  
+     if(vwoonIn < 13.20)  {
+        Display.Send(String(vwoonIn, 1), "Vwoon", "63488"); 
+       } else {
+        Display.Send(String(vwoonIn, 1), "Vwoon", "2016");        
+       }    
 
-//  // Uitlezen woonaccuspanning
-//   woonaccuSensorVal = analogRead(woonaccuSensorPin);       // lees de waarde van de sensor (0 - 1023) 
-//   vwoonOut = (woonaccuSensorVal / 1024) * vCC;             // converteer de gelezen waarde naar het daadwerkelijke voltage op de analoge pin
-//   vwoonIn =  vwoonOut * factorwoonaccu;  
-//     Serial.print("Woonaccu  ");Serial.print(vwoonIn);
-//     Serial.println("V");                                   // converteer het voltage naar het voltage aan de bron door te vermenigvuldigen met de factor
-//
-//  // Uitlezen startaccuspanning
-//   startaccuSensorVal = analogRead(startaccuSensorPin);       // lees de waarde van de sensor (0 - 1023) 
-//   vstartOut = (startaccuSensorVal / 1024) * vCC;             // converteer de gelezen waarde naar het daadwerkelijke voltage op de analoge pin
-//   vstartIn =  vstartOut * factorstartaccu;                   // converteer het voltage naar het voltage aan de bron door te vermenigvuldigen met de factor
-//     Serial.print("Startaccu ");Serial.print(vstartIn);
-//     Serial.println("V");
-//
-//  Uitlezen walstroom
+
+//       Laden / ontladen woonaccu
+        AwoonIn = analogRead(IwooninPin);
+        AwoonOut = analogRead(IwoonoutPin);
+       if(AwoonOut == AwoonIn)  {
+        Display.Send(String("-.-"), "Iwoon", "65504");
+       } else if(AwoonIn > AwoonOut) {
+        Display.Send(String((AwoonIn-AwoonOut) * 0.0048, 1), "Iwoon", "2016");
+       } else {
+        Display.Send(String((AwoonOut-AwoonIn) * -0.0048, 1), "Iwoon", "63844");
+       }
+        
+//       Startaccuspanning
+       startaccuSensorVal = analogRead(startaccuSensorPin);       // lees de waarde van de sensor (0 - 1023) 
+       vstartOut = (startaccuSensorVal / 1024) * vCC;             // converteer de gelezen waarde naar het daadwerkelijke voltage op de analoge pin
+       vstartIn =  vstartOut * factorstartaccu;  
+      if(vstartIn < 13.20)  {
+        Display.Send(String(vstartIn, 1), "Vstart", "63488"); 
+       } else {
+        Display.Send(String(vstartIn, 1), "Vstart", "2016");        
+  }    
+               
+//    Walstroom
       WalstroomState = digitalRead(WalstroomPin);
       if (WalstroomState == HIGH) {
-        Serial.print("Wstroom.bco=2016");
-        Serial.write(0xff);
-        Serial.write(0xff);
-        Serial.write(0xff);
+        Display.SendBCO("Wstroom", "2016");       
       } else {
-        Serial.print("Wstroom.bco=63488");
-        Serial.write(0xff);
-        Serial.write(0xff);
-        Serial.write(0xff);
-      }
-
+        Display.SendBCO("Wstroom", "63488");
+    }
+     
+//   Temeratuur & Luchtvochtigheid
+     byte temperaturebinnen;
+     byte humiditybinnen;
+     byte databinnen[40] = {0}; 
+        dht11.read(pinDHT11binnen, &temperaturebinnen, &humiditybinnen, databinnen);
+        Display.Send((int)temperaturebinnen, "Tbinnen");
+        Display.Send((int)humiditybinnen, "Hbinnen");
       
-//   Uitlezen temeratuur & Luchtvochtigheid
-  byte temperaturebinnen;
-  byte humiditybinnen;
-  byte databinnen[40] = {0};
-  dht11.read(pinDHT11binnen, &temperaturebinnen, &humiditybinnen, databinnen);
-  Display.Send((int)temperaturebinnen-2, "Tbinnen");
-  Display.Send((int)humiditybinnen, "Hbinnen");
-      
-  byte temperaturebuiten;
-  byte humiditybuiten;
-  byte databuiten[40] = {0};
-  dht11.read(pinDHT11buiten, &temperaturebuiten, &humiditybuiten, databuiten);
-
-  Display.Send((int)temperaturebuiten-2, "Tbuiten");
-  Display.Send((int)humiditybuiten, "Hbuiten");
+     byte temperaturebuiten;
+     byte humiditybuiten;
+     byte databuiten[40] = {0};
+        dht11.read(pinDHT11buiten, &temperaturebuiten, &humiditybuiten, databuiten);  
+        Display.Send((int)temperaturebuiten, "Tbuiten");
+        Display.Send((int)humiditybuiten, "Hbuiten");
   
-  //  //Uitlezen Clock RTC
-  Display.Send(rtc.getTimeStr(FORMAT_SHORT), "Time");
+//    Clock RTC
+        Display.Send(rtc.getTimeStr(FORMAT_SHORT), "Time");
+
+//    LPG tank
+          gaslevel = analogRead(gaslevelPin);
+          gasnivo = (gaslevel);
+          Display.Send((int)gasnivo / 46.5, "LPG");
+
+//    Koelkastventielatie
+       if ((int)temperaturebuiten >= 30) {
+          digitalWrite(KkastPin, HIGH);       
+      } else {
+          digitalWrite(KkastPin, LOW);       
+    }
   
  delay(1000);
-
+ 
 }
